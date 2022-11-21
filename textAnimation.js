230e1562107d30633e1c200d03e5736daa3bcdf1
Dhruv Ramaswamy <dhruv.ramaswamy@gmail.com>
2022-11-20 23:03:44 -0500

    First draft of text animation, is a good animation but is not resuable yet. Next commit will fix a bug

---
+++ textAnimation.js
@@ -0,0 +1,32 @@
+"use strict"
+// HOW LONG YOU WANT THE SPACE TO BE:
+let spaceBetweenNextLetterInMilliseconds = 50
+const text = Array.from(document.querySelectorAll(".text-animation-one"))
+convertTextOfElementToSpans(text[0])
+const spansOfText = Array.from(document.querySelector(".text-animation-one").children)
+console.log(spansOfText)
+function convertTextOfElementToSpans(element){
+    // This copies by value
+    let textToCreate = element.textContent.slice()
+    element.textContent = ""
+    for (let character of textToCreate){
+        // innerHtml and text content are different here
+        element.innerHTML += `<span>${character}</span>`
+    }
+    return element.innerHTML;
+}
+
+const clear = setInterval(triggerAnimation, spaceBetweenNextLetterInMilliseconds)
+let counter = 0;
+let givenSpan;
+function triggerAnimation(){
+    givenSpan = spansOfText[counter]
+    givenSpan.classList.add('active')
+    counter++;
+    if(counter >= spansOfText.length){
+        clearInterval(clear)
+    }
+}
+
+
+
